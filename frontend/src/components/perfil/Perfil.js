5768b760816dcb9d1927cf861c5c257de17b8a7c
Pedro-Rocha-Araujo <pedrorochaaraujo82@gmail.com>
2026-05-09 08:40:03 -0300

    Página de perfil mais elaborada

--- frontend/src/components/perfil/Perfil.js
+++ frontend/src/components/perfil/Perfil.js
@@ -1,6 +1,8 @@
+import { useState } from "react"
 import "./perfil.css"
 
 function Perfil() {
+  const [minhasCasas, setMinhasCasas] = useState([])
   return (
     <div className="container">
       <div className="dados">
@@ -10,8 +12,13 @@ function Perfil() {
       <div className="casas-perfil">
         <h2><i className="fa-solid fa-house"></i> Suas Casas</h2>
         <div className="casas-cadastradas">
-
+          { minhasCasas.length === 0 ? (
+            <p>Nenhuma casa Cadastrada ainda!</p>
+          ): (
+            <p>Cas</p>
+          ) }
         </div>
+        <button className="cadastrar">Cadastrar casa</button>
       </div>
       <button className="sair">Sair</button>
     </div>
