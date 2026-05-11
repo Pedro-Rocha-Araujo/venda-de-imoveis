83c9e47d9058601ab3962a3a1d471d4c268e9acd
Pedro-Rocha-Araujo <pedrorochaaraujo82@gmail.com>
2026-05-11 10:08:28 -0300

    Rota de login implementada

--- frontend/src/components/usuario/Login.js
+++ frontend/src/components/usuario/Login.js
@@ -1,7 +1,28 @@
 import "./usuario.css"
-import { Link } from "react-router-dom"
+import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import { toast } from "react-toastify"
+import axios from "axios"
 
 function Login() {
+  const [email, setEmail] = useState("")
+  const navigate = useNavigate()
+
+  async function fazerLogin(e) {
+    e.preventDefault() 
+    try {
+      const response = await axios.post("http://localhost:4000/login", {
+        email
+      })
+      const token = response.data.token
+      localStorage.setItem("token", token)
+      toast.success("Login feito com sucesso!")
+      navigate("/casas")
+    } catch {
+      toast.error("Erro ao logar!")
+    }
+  }
+
   return (
     <div className="container usuario">
 
@@ -9,8 +30,14 @@ function Login() {
         <div className="header-cadastro">
           <h2>Login</h2>
         </div>
-        <form className="main-cadastro">
-          <input type="email" placeholder="Digite seu Email" required />
+        <form onSubmit={fazerLogin} className="main-cadastro">
+          <input 
+            type="email" 
+            placeholder="Digite seu Email" 
+            required 
+            name="email" value={email}
+            onChange={(e)=>setEmail(e.target.value)}
+          />
           <button>Logar</button>
         </form>
         <p>Não tem uma conta?<Link to="/cadastro"> Fazer Cadastro.</Link></p>
